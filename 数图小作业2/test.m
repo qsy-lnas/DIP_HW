0e1d57bcdffbd8292e8de17c70acad840535ea21
qsy-lnas <lnasqsy@qq.com>
2021-10-07 22:00:59 +0800

    example_over

--- 数图小作业2/test.m
+++ 数图小作业2/test.m
@@ -3,16 +3,8 @@ b  =1;
 if b == 1
 
     image = imread("lounge-hdr-foreground.png");
-    [~, ~, alpha] = imread("lounge-hdr-foreground.png");
+    [image__, ~, alpha] = imread("lounge-hdr-foreground.png");
     image_ = imread("lounge-hdr-background.png");
 end
-image_scale = image > 0;
-image_scale_ = image == 255;
-i = image(:, :, 1);
-j = image_scale(:, :, 1);
-k = image_scale_(:, :, 1);
-s = alpha;
-image_processed = background.*uint8(~forescope) + foreground;
-a = size(image(:, :, 1));
-image_scale(200, :)
-imshow(uint8(image_scale) .* image_);
+
+imshow(image__);
