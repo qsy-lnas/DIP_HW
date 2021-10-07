f6546c4e82ae9b575b3a3353498e85db1124b54f
qsy-lnas <lnasqsy@qq.com>
2021-10-07 20:56:23 +0800

    cat_edge_disappear

--- 数图小作业2/test.m
+++ 数图小作业2/test.m
@@ -2,12 +2,17 @@
 b  =1;
 if b == 1
 
-    [image, ~, alpha] = imread("lounge-hdr-foreground.png");
+    image = imread("lounge-hdr-foreground.png");
+    [~, ~, alpha] = imread("lounge-hdr-foreground.png");
     image_ = imread("lounge-hdr-background.png");
 end
 image_scale = image > 0;
-j = image(:, :, 1);
-i = ~image(:, :, 1);
+image_scale_ = image == 255;
+i = image(:, :, 1);
+j = image_scale(:, :, 1);
+k = image_scale_(:, :, 1);
+s = alpha;
+image_processed = background.*uint8(~forescope) + foreground;
 a = size(image(:, :, 1));
 image_scale(200, :)
 imshow(uint8(image_scale) .* image_);
