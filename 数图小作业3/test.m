b922f8f52849793c1b4100dd6ce2a92be4dd8fb0
qsy-lnas <lnasqsy@qq.com>
2021-10-13 01:01:21 +0800

    HW3_Rectangle_origin

--- 数图小作业3/test.m
+++ 数图小作业3/test.m
@@ -1,7 +1,15 @@
 an = 0;
 ph = 0;
 fr = 0.002;
+sigma = 10;
 [X, Y] = meshgrid(1 : 256);
-im = cos(2 .* pi .* fr .* (cos(an) * X + sin(an) * Y) + ph);
+sum = im(128, 128) + im(128, 129) + ...
+    im(129, 128) + im(129, 129);
+sum = sum ./ 4;
+a = 1 ./ sum;
+im = (X - 128.5) .^ 2 + (Y - 128.5) .^ 2;
+im = - im ./ (2 .* sigma .^ 2);
+im = exp(im) ./ (2 .* pi .* sigma .^ 2);
+im = im .* a .* 10;
 imshow(im);
-pi
+
